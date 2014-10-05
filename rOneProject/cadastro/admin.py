f328fe7735517e54e717c8f24dbc5153f51e71fc
rodrigolucianocosta <rodrigolucianocosta@hotmail.com>
2014-10-05 17:08:10 -0300

    criacao do modelo ficha de matricula e alteracao para mostrar dados

--- rOneProject/cadastro/admin.py
+++ rOneProject/cadastro/admin.py
@@ -1,3 +1,13 @@
+# _*_ coding utf-8 _*_
 from django.contrib import admin
-
+from models import ficha_de_matricula
 # Register your models here.
+
+class displayNome(admin.ModelAdmin):
+	list_display = ['nome_do_aluno','dia_Atendimento','horario_atendimento']
+	list_filter = ['nome_do_aluno']
+	search_fields = ['cid']
+	save_as = True 
+
+
+admin.site.register(ficha_de_matricula,displayNome)
--- rOneProject/cadastro/models.py
+++ rOneProject/cadastro/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class ficha_de_matricula(models.Model): 
+	cid  = models.CharField('cid',max_length=100) 
+	nome_do_aluno = models.CharField(max_length=100) 
+	cartao_sus = models.CharField(max_length=20) 
+	cpf = models.CharField(max_length=11) 
+	responsavel_pelo_aluno = models.CharField(max_length=100) 
+	rua = models.CharField(max_length=100) 
+	numero = models.IntegerField() 
+	bairro = models.CharField(max_length=100) 
+	cidade = models.CharField(max_length=100) 
+	data_de_nascimento = models.DateField() 
+	naturalidade = models.CharField(max_length=50) 
+	idade = models.CharField(max_length=2) 
+	sexo = models.CharField(max_length=1) 
+	cor = models.CharField(max_length=10) 
+	nome_do_Pai = models.CharField(max_length=100) 
+	profissao_do_Pai = models.CharField(max_length=50) 
+	nome_da_Mae = models.CharField(max_length=100) 
+	profissao_da_Mae = models.CharField(max_length=50) 
+	email = models.EmailField() 
+	dia_Atendimento = models.CharField(max_length=100) 
+	horario_atendimento = models.DateTimeField('horario') 
+	data_matricula = models.DateField() 
+	 
+	def __unicode__(self): 
+		return self.ficha_de_matricula 
